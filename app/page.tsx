4ab487e67927465a0dc9509c698dc6a2f6cfad5f
rotarymars <s.goto2050@gmail.com>
2026-04-30 05:57:04 +0900

    fixing the order

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Toppage() {
     <>
       <header className={styles.header}>
         <br /><br /><br /><br /><br /><br />
+        <h1 className={styles.theme}>青、薫る</h1>
         <h2 className={styles.top}>
           2026行事週間 <br />
           9/7~9/14
         </h2>
-        <h1 className={styles.theme}>青、薫る</h1>
         <p className={styles.scroll}>Scroll</p>
 
         <svg className={styles.curveLine} viewBox="0 0 100 500" aria-hidden="true" focusable={false}>
