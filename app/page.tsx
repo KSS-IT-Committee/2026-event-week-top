9ad0d9d53e8146ddf31a1dceaad8cd0c107b9a17
rotarymars <s.goto2050@gmail.com>
2026-05-08 13:25:29 +0900

    exporting the footer as a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { defaultFooter } from "@/app/components/footer";
 import styles from "./top-page.module.css";
 
 export default function Toppage() {
@@ -134,11 +135,7 @@ export default function Toppage() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <h1 className={styles.footerTheme}>行事週間2026 青、薫る</h1>
-        <p>© 2026 小石川中等教育学校</p>
-        <p>行事運営委員会・IT委員会</p>
-      </footer>
+      {defaultFooter()}
     </>
   );
 }
