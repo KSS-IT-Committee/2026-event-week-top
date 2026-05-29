ee017471577376f258b8d00a2b95ec3f275f15cc
rotarymars <s.goto2050@gmail.com>
2026-05-29 19:22:22 +0900

    formatting files

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,9 @@ export default function Toppage() {
                 <time className={styles.newsDate} dateTime="2026-05-24">
                   2026/05/24
                 </time>
-                <span className={styles.newsText}>現在特に連絡はありません</span>
+                <span className={styles.newsText}>
+                  現在特に連絡はありません
+                </span>
               </li>
             </ul>
           </div>
