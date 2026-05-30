4c6cad52d75725271a301629a2adf37dbcef6327
utsukushiioto0816-tech <utsukushiioto0816@gmail.com>
2026-05-30 14:13:20 +0900

    add tag for each news

--- app/news/newsData.ts
+++ app/news/newsData.ts
@@ -3,10 +3,12 @@ export const news = [
     id: "news1",
     title: "予備大の日程が公開されました",
     date: "2026-05-24",
+    tag: "sport"
   },
   {
     id: "news2",
     title: "予備大ドッジボールの結果",
     date: "2026-05-29",
+    tag: "sport"
   }
 ]
