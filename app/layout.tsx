c0ae1cd80c3906330aecfafc8b3b626161ff77d1
rotarymars <s.goto2050@gmail.com>
2026-06-10 22:02:55 +0900

    forcing users to use javascript

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 
+import { NoScriptAlert } from "@/components/NoScriptAlert";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -49,7 +51,10 @@ gtag('config', 'G-STVFHMQS05');`}
           </>
         )}
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NoScriptAlert />
+        {children}
+      </body>
     </html>
   );
 }
