6aca9c27f61c6030fa8c72f322406eebf95a0807
rotarymars <s.goto2050@gmail.com>
2026-07-12 23:44:48 +0900

    not showing lottery page on the menu yet

--- app/page.tsx
+++ app/page.tsx
@@ -311,7 +311,7 @@ export default function Toppage() {
           { label: "体育祭", href: "#sports" },
           { label: "創作展", href: "#create" },
           { label: "後夜祭", href: "#ceremony" },
-          { label: "観覧抽選", href: "/lottery", isInternal: true },
+          // { label: "観覧抽選", href: "/lottery", isInternal: true },
           { label: "News", href: "/news/list" },
           { label: "ページ改善の提案", href: "/requests" },
           { label: "Changelog", href: "/changelog" },
