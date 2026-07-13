6391af7ec8c966f8a66016dc859483fae0ff6146
utsukushiioto0816-tech <utsukushiioto0816@gmail.com>
2026-07-13 23:28:44 +0900

    add content

--- app/components/Countdown/index.tsx
+++ app/components/Countdown/index.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from "react";
 
 export default function Countdown({ targetDate }: { targetDate: string }) {
-  const [weeks, setWeeks] = useState(0);
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -16,24 +15,20 @@ export default function Countdown({ targetDate }: { targetDate: string }) {
       const diff = targetData.getTime() - now.getTime();
 
       if (diff <= 0) {
-        setWeeks(0);
         setDays(0);
         setHours(0);
         setMinutes(0);
         setSeconds(0);
-        return;
+        return "行事週間スタート!!";
       }
 
       const sec = Math.floor(diff / 1000);
-      const weeksVal = Math.floor(sec / 604800);
-      const secAfterWeeks = sec % 604800;
-      const daysVal = Math.floor(secAfterWeeks / 86400);
-      const secAfterDays = secAfterWeeks % 86400;
+      const daysVal = Math.floor(sec / 86400);
+      const secAfterDays = sec % 86400;
       const hoursVal = Math.floor(secAfterDays / 3600);
       const secAfterHours = secAfterDays % 3600;
       const minutesVal = Math.floor(secAfterHours / 60);
       const secondsVal = secAfterHours % 60;
-      setWeeks(weeksVal);
       setDays(daysVal);
       setHours(hoursVal);
       setMinutes(minutesVal);
