f8fd6c4e29e15234c56887735c6d4b96ba18044d
utsukushiioto0816-tech <utsukushiioto0816@gmail.com>
2026-07-13 23:25:54 +0900

    fix conflict

--- app/components/Countdown/index.tsx
+++ app/components/Countdown/index.tsx
@@ -2,8 +2,10 @@
 import { useEffect, useState } from "react";
 
 export default function Countdown({ targetDate }: { targetDate: string }) {
-  const [time, setTime] = useState("");
+  const [weeks, setWeeks] = useState(0);
+  const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
@@ -14,32 +16,41 @@ export default function Countdown({ targetDate }: { targetDate: string }) {
       const diff = targetData.getTime() - now.getTime();
 
       if (diff <= 0) {
-        setTime("行事週間スタート‼");
+        setWeeks(0);
+        setDays(0);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
         return;
       }
+
       const sec = Math.floor(diff / 1000);
-      const weeks = Math.floor(sec / 604800);
+      const weeksVal = Math.floor(sec / 604800);
       const secAfterWeeks = sec % 604800;
-      const days = Math.floor(secAfterWeeks / 86400);
+      const daysVal = Math.floor(secAfterWeeks / 86400);
       const secAfterDays = secAfterWeeks % 86400;
-      const hours = Math.floor(secAfterDays / 3600);
+      const hoursVal = Math.floor(secAfterDays / 3600);
       const secAfterHours = secAfterDays % 3600;
       const minutesVal = Math.floor(secAfterHours / 60);
       const secondsVal = secAfterHours % 60;
-      setHours(hours);
+      setWeeks(weeksVal);
+      setDays(daysVal);
+      setHours(hoursVal);
+      setMinutes(minutesVal);
       setSeconds(secondsVal);
-      setTime(
-        `${weeks}週間 ${days}日 ${hours}時間 ${minutesVal}分 ${secondsVal}秒`,
-      );
     };
+
     update();
     const interval = setInterval(update, 1000);
     return () => clearInterval(interval);
   }, [targetDate]);
 
   return (
-    <div className="text-center text-2xl font-bold">
-      <div className="mb-4">{time}</div>
+    <div className="flex gap-4 text-center text-2xl font-bold">
+      <div>{days}日</div>
+      <div>{hours}時間</div>
+      <div>{minutes}分</div>
+      <div>{seconds}秒</div>
     </div>
   );
 }
