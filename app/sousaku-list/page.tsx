0620ad98d038edc28599854b8f8684e2f90ca7f0
utsukushiioto0816-tech <utsukushiioto0816@gmail.com>
2026-08-19 22:00:13 +0900

    add Metadata and fixed content

--- app/sousaku-list/page.tsx
+++ app/sousaku-list/page.tsx
@@ -1,10 +1,16 @@
 import { readFile } from "node:fs/promises";
 import path from "node:path";
 
+import { Metadata } from "next";
 import Link from "next/link";
 
 import styles from "@/app/sousaku-list/sousaku-list.module.css";
 import { parseMarkdown } from "@/lib/markdown";
+
+export const metadata: Metadata = {
+  title: "創作部門 | 劇内容紹介",
+  description: "創作部門の各クラスの劇の内容紹介ページ",
+};
 
 export default async function SousakuListPage() {
   const content = await readFile(
